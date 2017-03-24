14b61212451d748f7a95733c79899d0274001057
Matheus Lima <liminha.matheus@gmail.com>
2017-03-24 18:39:40 -0300

    - Scrips de Thyago incluídos

---
+++ RanAto/Assets/Scripts/ColetaDeItens.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColetaDeItens : MonoBehaviour {
+	//Quantidade de moedas
+	public int moedas;
+
+	//Verifica a Colisao se o jogador colidiu com algum item e seleciona o efeito.
+	void OnCollisionEnter2D(Collision2D coll) {
+		
+		string nome = coll.gameObject.name;
+		//Faz uma selecao de qual item foi coletado e chama a funcao correspondente.
+		switch (nome){
+
+		//seleciona o Moedas e executa a funcao.
+		case "Moedas":
+			moedas++;
+            break;
+		}
+		//Esperando efeitos dos itens para colocar os codigos.
+		//Esperando Estudo de dados persistentes para corrigir o codigo de maneira melhor.
+
+	}
+}
