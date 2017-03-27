61e8e2995103924713270302f531597aa3a02e53
Matheus Lima <liminha.matheus@gmail.com>
2017-03-27 18:36:46 -0300

    Script e cena de seleção de personagem incluído (Maracajá) (clique p/ mais detalhes) - O script não vai funcionar no estado atual. Verificar Issue no GitHub. - Tamanho da tela incorreto. Corrigir.

---
+++ RanAto/Assets/Scripts/Selecionar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecionar : MonoBehaviour {
+
+    // O script abaixo não vai funcionar na versão Mobile.
+    // Preciso que tu olhe como funciona o touch no jogo principal e tente implementar aqui (é simples).
+    public GameObject samurai;
+    public GameObject ninja;
+    public GameObject menina;
+    public int escolha;
+
+    public void OnMouseDown(GameObject outro)
+    {
+
+        if (outro.CompareTag("Samurai"))
+        {
+            escolha = 1;
+        }
+
+        else if (outro.CompareTag("Ninja"))
+        {
+            escolha = 2;
+        }
+
+        else if (outro.CompareTag("Menina"))
+        {
+            escolha = 3;
+        }
+    }
+}
