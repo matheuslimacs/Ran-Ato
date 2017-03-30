1241e632f4cd30cc842115ebe84f61a4dc549ed4
taryel <thiago.the.gaspar@hotmail.com>
2017-03-30 16:39:21 -0300

    Criação da classe ControladorDados.
    Classe Criada para substituir SalvarPontuação, pois ela ficará
    responsavel por todo o gerenciamento de dados salvos no jogo.
    Feito a classe de controlar e salvar os dados do jogo como Moedas e
    Distancia máxima, foi testada e funciona porém ainda precisa solucionar
    a melhor forma de interação com o jogo para evitar criar o máximo de
    objetos possiveis.

---
+++ RanAto/Assets/Scripts/ControladorDados.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDados : ScriptableObject {
+
+	//Variaveis contendo os dados.
+	private  int tMoedas;
+	private  int distancia;
+	private  int mDistancia;
+
+	//Propriedade da distancia.
+	public int Distancia{
+		get{
+			return distancia;
+		}
+		set{ 
+			distancia = value;
+		}
+	}
+
+	//Propriedade das moedas
+	public int TMoedas{
+		get{
+			return tMoedas;
+		}
+		set{ 
+			tMoedas = value;
+		}
+	}
+
+	// Funcão cridada para atualizar os dados da tela.
+	//Essa função deverá ser chamada todo final de fase.
+	public static void CarregaDados(){
+		mDistancia = PlayerPrefs.GetInt ("mDistancia");
+		tMoedas = PlayerPrefs.GetInt ("tMoedas");
+	}
+
+	//Função cridada para verificar e salvar os dados das moedas e da maior distância.
+	public static void SalvaDados ()
+	{
+		if (distancia >= mDistancia) {
+			mDistancia = distancia;
+			PlayerPrefs.SetInt ("mDistancia", mDistancia);
+		}
+		PlayerPrefs.SetInt ("tMoedas",tMoedas);
+	}
+}
