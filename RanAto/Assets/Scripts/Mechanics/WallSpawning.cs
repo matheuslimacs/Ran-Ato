738efd8f2a59a0f0ce02817c8c363e9da0e76233
Matheus11469 <liminha.matheus@gmail.com>
2017-04-03 17:28:48 -0300

    Assets do chão e paredes colocados

---
+++ RanAto/Assets/Scripts/Mechanics/WallSpawning.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawning : MonoBehaviour {
+
+    // Esse script irá posicionar corretamente cada plataforma na frente da outra.
+
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    public ObjectPooler objPool;
+
+    private void Start()
+    {
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x - platformWidth, transform.position.y, transform.position.z);
+
+            GameObject newPlatform = objPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+        }
+    }
+}
